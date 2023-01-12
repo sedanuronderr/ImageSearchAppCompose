fc75eebf67b3d75c8a3ac4b009070cd6098dd335
Seda Nur Önder <sednrondr@gmail.com>
2023-01-12 15:41:50 +0300

    commit

--- app/src/main/java/com/seda/imagesearchapp_compose/utils/Constants.kt
+++ app/src/main/java/com/seda/imagesearchapp_compose/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.seda.imagesearchapp_compose.utils
 object Constants {
     const val END_POINT: String = "api/"
     const val BASE_URL: String = "https://pixabay.com/"
-    const val KEY:String ="28415526-a0b249b1e5eacee446523661d"
+    const val KEY:String ="Key"
 
 }
